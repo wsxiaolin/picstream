f6792abd9c5c77ea80286c7b5d0bae2e335615d7
hello8693 <1320998105@qq.com>
2024-10-07 15:35:11 +0800

    Update vite.config.ts to configure base path for classisland-hub-web deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools()],
+  base: '/classisland-hub-web/', // 确保这里的路径与您的仓库名称匹配
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
